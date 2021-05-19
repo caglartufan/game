0ba1bb343986c015aa2e00e53b66c4adee2736bc
n3pixowe <n3pixowe@gmail.com>
2021-05-19 19:59:35 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const createError = require('http-errors');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const debug = require('debug')('game:app');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+const server = http.createServer(app);
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+var players = [];
+
+// view engine setup
+app.set('views', path.join(process.cwd(), 'views'));
+app.set('view engine', 'pug');
+
+//app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+app.use('/', indexRouter);
+
+io.on('connection', (socket) => {
+    debug(`${socket.id} connected!`);
+
+    socket.on('player joined', (player) => {
+        players.push(player);
+        io.emit('player joined', player);
+        socket.emit('init players', players);
+    });
+
+    socket.on('update', (player) => {
+        let index = players.findIndex((p) => p.id === player.id);
+        players[index] = player;
+        io.emit('update', players);
+    });
+
+    socket.on('disconnect', (reason) => {
+        debug(`${socket.id} disconnected!`);
+        players = players.filter((player) => player.id !== socket.id);
+        io.emit('player left', socket.id);
+    });
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = {
+    app,
+    server
+};
---
+++ public/javascripts/main.js
@@ -0,0 +1,137 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+var width, height;
+var lastRender;
+var keyMap = {
+    65: 'left',
+    68: 'right',
+    87: 'up',
+    83: 'down'
+};
+var players = [];
+
+const socket = io();
+
+socket.on('connect', () => {
+    newPlayer(socket.id);
+});
+
+socket.on('init players', (playersConnected) => {
+    players = playersConnected;
+    lastRender = 0;
+    window.requestAnimationFrame(loop);
+});
+
+socket.on('update', (playersConnected) => {
+    players = playersConnected;
+});
+
+socket.on('player joined', (player) => {
+    if(player.id !== socket.id) {
+        players.push(player);
+        console.log(socket.id + ' joined!');
+    }
+});
+
+socket.on('player left', (playerId) => {
+    players = players.filter((player) => player.id !== playerId);
+    console.log(playerId + ' left!');
+});
+
+const resize = function() {
+    //width = window.innerWidth;
+    //height = window.innerHeight;
+    width = 700;
+    height = 700;
+    canvas.width = width;
+    canvas.height = height;
+}
+
+function keydown(event) {
+    let key = keyMap[event.keyCode];
+    if(key) {
+        let player = players.find((p) => p.id === socket.id);
+        player.pressedKeys[key] = true;
+        socket.emit('update', player);
+    }
+}
+
+function keyup(event) {
+    let key = keyMap[event.keyCode];
+    if(key) {
+        let player = players.find((p) => p.id === socket.id);
+        player.pressedKeys[key] = false;
+        socket.emit('update', player);
+    }
+}
+
+window.onresize = resize;
+window.addEventListener('keydown', keydown, false);
+window.addEventListener('keyup', keyup, false);
+
+resize();
+
+function newPlayer(id) {
+    let player = {
+        id: id,
+        colors: Array.from({ length: 3 }, () => Math.floor(Math.random()*256)),
+        state: {
+            x: Math.floor(Math.random()*676),
+            y: Math.floor(Math.random()*676)
+        },
+        pressedKeys: {
+            left: false,
+            right: false,
+            up: false,
+            down: false
+        }
+    };
+    players.push(player);
+    socket.emit('player joined', player);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, width, height);
+    players.forEach((player) => {
+        ctx.fillStyle = `rgb(${player.colors[0]}, ${player.colors[1]}, ${player.colors[2]})`;
+        ctx.fillRect(player.state.x, player.state.y, 10, 10);
+    });
+}
+
+function update(progress) {
+    let player = players.find((p) => p.id === socket.id);
+    if(player.pressedKeys.left) {
+        player.state.x -= progress;
+    }
+    if(player.pressedKeys.right) {
+        player.state.x += progress;
+    }
+    if(player.pressedKeys.up) {
+        player.state.y -= progress;
+    }
+    if(player.pressedKeys.down) {
+        player.state.y += progress;
+    }
+
+    if(player.state.x > width) {
+        player.state.x -= width;
+    } else if(player.state.x < 0) {
+        player.state.x += width;
+    } else if(player.state.y > height) {
+        player.state.y -= height;
+    } else if(player.state.y < 0) {
+        player.state.y += height;
+    }
+
+    socket.emit('update', player);
+}
+
+function loop(timestamp) {
+    let progress = timestamp - lastRender;
+    if(socket.connected) {
+        update(progress);
+        draw();
+    }
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop);
+}
